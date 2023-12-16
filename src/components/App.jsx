53b398b4038d5e8358288834f9ad28be6388a30d
MariiaSam <kontragent@ukr.net>
2023-12-16 21:11:11 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyledApp } from './App.styled';
 
 import { getImages } from 'serviсes/getImages';
@@ -10,148 +10,73 @@ import { ImageGallery } from './ImageGallery/ImageGallery';
 
 import { Button } from './Button/Button';
 
-export class App extends Component {
-  state = {
-    query: '', //Зберігає поточний запит для пошуку.
-    gallery: [],
-    page: 1,
-    error: null,
-    isLoading: false,//Вказує, чи в даний момент відбувається завантаження зображень.
-    showModal: false,
-    largeImage: '',
-    loadMore: false,//Вказує, чи є ще зображення для завантаження
-  };
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [gallery, setGallery] = useState([]);
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImage, setLargeImage] = useState('');
+  const [loadMore, setLoadMore] = useState(false);
 
-  async componentDidUpdate(_, prevState) {
-    try {
-      const { query, page } = this.state;
-      const prevQuery = prevState.query;
-      const prevPage = prevState.page;
+  useEffect(() => {
+    if (query === '') return;
 
-      if (query !== prevQuery || page !== prevPage) {
-        await this.createImgGallery();//Якщо відбулася зміна, він викликає метод createImgGallery.
+    setIsLoading(true);
 
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+    getImages(query, page)
+      .then(({ hits, totalHits }) => {
+        if (hits.length === 0) {
+          setGallery([]);
+          setError('Invalid input. Please enter a valid value.');
+          return;
+        }
 
-  handleSearch = query => {
-    this.setState({
-      query,
-      gallery: [],
-      page: 1,
-    });
-  };
+        setGallery((prevState) => [...prevState, ...hits]);
+        setLoadMore(page < Math.ceil(totalHits / 12));
+        setError(null);
+      })
 
-  createImgGallery = async () => {
-    this.setState({ isLoading: true });
-    const { query, page } = this.state;
-
-    try {
-      const { hits, totalHits } = await getImages(query, page);
-
-      console.log(totalHits)
-
-      if (hits.length === 0) {
-        this.setState({
-          gallery: [],
-          error: 'Invalid input. Please enter a valid value.',
-        });
-        return;
-      }
-
-      this.setState(prevState => ({
-        gallery: [...prevState.gallery, ...hits],
-        loadMore: this.state.page < Math.ceil(totalHits / 12),
-        error: null,
-      }));
-    } catch (error) {
-      console.log(error);
-      this.setState({
-        gallery: [],
-        error: 'Message Err',
+      .catch((error) => {
+        console.log(error);
+        setGallery([]);
+        setError('Message Err');
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
-    } finally {
-      this.setState({ isLoading: false });
-    }
+  }, [query, page]);
+
+  const handleSearch = (query) => {
+    setQuery(query);
+    setGallery([]);
+    setPage(1);
+  };
+  
+  const onLoadMoreImage = () => {
+    setPage(page => page + 1);
   };
 
-  onLoadMoreImage = () => {
-    this.setState(({ page }) => ({ page: page + 1 }));
-  };
-
-  onOpenModal = largeImage => {
+  const onOpenModal = largeImage => {
     if (largeImage) {
-      this.setState({ largeImage });
-      this.toggleModal();
+      setLargeImage({ largeImage });
+      toggleModal();
     }
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal, //Змінює стан showModal між true і false
-    }));
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
-  render() {
-    const { isLoading, showModal, largeImage, gallery, error, loadMore } =
-      this.state;
-
-    return (
-      <StyledApp>
-        <SearchBar onSubmit={this.handleSearch} />
-        <ImageGallery galleryImg={gallery} onClick={this.onOpenModal} />
-        {isLoading && <Loader />}
-        {showModal && <Modal onClick={this.toggleModal} url={largeImage} />}
-        {error !== null ? <div>{error}</div> : null}
-        {loadMore && <Button onLoadMore={this.onLoadMoreImage} />}
-      </StyledApp>
-    );
-  }
-}
-
-
-//query: Зберігає поточний запит для пошуку.
-// gallery: Зберігає масив отриманих зображень від API.
-// page: Визначає поточну сторінку результатів.
-// error: Містить повідомлення про помилку, пов'язану з пошуком.
-// isLoading: Вказує, чи в даний момент відбувається завантаження зображень.
-// showModal: Відповідає за видимість модального вікна.
-// largeImage: Містить URL великого зображення для відображення в модальному вікні.
-// loadMore: Вказує, чи є ще зображення для завантаження.
-
-
-//componentDidUpdate
-// Цей метод життєвого циклу викликається кожного разу, коли компонент оновлюється.
-// Він порівнює поточні значення query і page з попередніми значеннями.
-// Якщо відбулася зміна, він викликає метод createImgGallery.
-
-
-//handleSearch:
-// Скидає стан при новому запиті.
-// Очищає поточний gallery, скидає page на 1 та оновлює query
-
-//createImgGallery:
-// Встановлює isLoading в true перед отриманням зображень.
-// Отримує зображення за допомогою функції getImages.
-// Якщо зображення не отримані, встановлює повідомлення про помилку.
-// Додає нові зображення до існуючого gallery.
-// Визначає, чи є ще сторінки для завантаження (loadMore).
-// Відловлює будь-які помилки під час процесу і встановлює повідомлення про помилку.
-// У блоку finally встановлює isLoading в false після завершення операції.
-
-
-//onLoadMoreImage:
-//Збільшує значення page, коли натискається кнопка "Завантажити ще".
-
-
-//onOpenModal:
-// Відкриває модальне вікно з вказаним URL largeImage.
-// Викликає toggleModal для оновлення стану showModal.
-
-
-//toggleModal
-
-//Метод рендеру:
+  return (
+    <StyledApp>
+      <SearchBar onSubmit={handleSearch} />
+      <ImageGallery galleryImg={gallery} onClick={onOpenModal} />
+      {isLoading && <Loader />}
+      {showModal && <Modal onClick={toggleModal} url={largeImage} />}
+      {error !== null ? <div>{error}</div> : null}
+      {loadMore && <Button onLoadMore={onLoadMoreImage} />}
+    </StyledApp>
+  );
+};
---
+++ src/components/AppCopy.jsx
@@ -0,0 +1,157 @@
+// import React, { Component } from 'react';
+// import { StyledApp } from './App.styled';
+
+// import { getImages } from 'serviсes/getImages';
+// import { SearchBar } from './Searchbar/Searchbar';
+// import { Loader } from './Loader/Loader';
+
+// import { Modal } from './Modal/Modal';
+// import { ImageGallery } from './ImageGallery/ImageGallery';
+
+// import { Button } from './Button/Button';
+
+// export class App extends Component {
+//   state = {
+//     query: '', //Зберігає поточний запит для пошуку.
+//     gallery: [],
+//     page: 1,
+//     error: null,
+//     isLoading: false,//Вказує, чи в даний момент відбувається завантаження зображень.
+//     showModal: false,
+//     largeImage: '',
+//     loadMore: false,//Вказує, чи є ще зображення для завантаження
+//   };
+
+//   async componentDidUpdate(_, prevState) {
+//     try {
+//       const { query, page } = this.state;
+//       const prevQuery = prevState.query;
+//       const prevPage = prevState.page;
+
+//       if (query !== prevQuery || page !== prevPage) {
+//         await this.createImgGallery();//Якщо відбулася зміна, він викликає метод createImgGallery.
+
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+
+//   handleSearch = query => {
+//     this.setState({
+//       query,
+//       gallery: [],
+//       page: 1,
+//     });
+//   };
+
+//   createImgGallery = async () => {
+//     this.setState({ isLoading: true });
+//     const { query, page } = this.state;
+
+//     try {
+//       const { hits, totalHits } = await getImages(query, page);
+
+//       console.log(totalHits)
+
+//       if (hits.length === 0) {
+//         this.setState({
+//           gallery: [],
+//           error: 'Invalid input. Please enter a valid value.',
+//         });
+//         return;
+//       }
+
+//       this.setState(prevState => ({
+//         gallery: [...prevState.gallery, ...hits],
+//         loadMore: this.state.page < Math.ceil(totalHits / 12),
+//         error: null,
+//       }));
+//     } catch (error) {
+//       console.log(error);
+//       this.setState({
+//         gallery: [],
+//         error: 'Message Err',
+//       });
+//     } finally {
+//       this.setState({ isLoading: false });
+//     }
+//   };
+
+//   onLoadMoreImage = () => {
+//     this.setState(({ page }) => ({ page: page + 1 }));
+//   };
+
+//   onOpenModal = largeImage => {
+//     if (largeImage) {
+//       this.setState({ largeImage });
+//       this.toggleModal();
+//     }
+//   };
+
+//   toggleModal = () => {
+//     this.setState(({ showModal }) => ({
+//       showModal: !showModal, //Змінює стан showModal між true і false
+//     }));
+//   };
+
+//   render() {
+//     const { isLoading, showModal, largeImage, gallery, error, loadMore } =
+//       this.state;
+
+//     return (
+//       <StyledApp>
+//         <SearchBar onSubmit={this.handleSearch} />
+//         <ImageGallery galleryImg={gallery} onClick={this.onOpenModal} />
+//         {isLoading && <Loader />}
+//         {showModal && <Modal onClick={this.toggleModal} url={largeImage} />}
+//         {error !== null ? <div>{error}</div> : null}
+//         {loadMore && <Button onLoadMore={this.onLoadMoreImage} />}
+//       </StyledApp>
+//     );
+//   }
+// }
+
+
+// //query: Зберігає поточний запит для пошуку.
+// // gallery: Зберігає масив отриманих зображень від API.
+// // page: Визначає поточну сторінку результатів.
+// // error: Містить повідомлення про помилку, пов'язану з пошуком.
+// // isLoading: Вказує, чи в даний момент відбувається завантаження зображень.
+// // showModal: Відповідає за видимість модального вікна.
+// // largeImage: Містить URL великого зображення для відображення в модальному вікні.
+// // loadMore: Вказує, чи є ще зображення для завантаження.
+
+
+// //componentDidUpdate
+// // Цей метод життєвого циклу викликається кожного разу, коли компонент оновлюється.
+// // Він порівнює поточні значення query і page з попередніми значеннями.
+// // Якщо відбулася зміна, він викликає метод createImgGallery.
+
+
+// //handleSearch:
+// // Скидає стан при новому запиті.
+// // Очищає поточний gallery, скидає page на 1 та оновлює query
+
+// //createImgGallery:
+// // Встановлює isLoading в true перед отриманням зображень.
+// // Отримує зображення за допомогою функції getImages.
+// // Якщо зображення не отримані, встановлює повідомлення про помилку.
+// // Додає нові зображення до існуючого gallery.
+// // Визначає, чи є ще сторінки для завантаження (loadMore).
+// // Відловлює будь-які помилки під час процесу і встановлює повідомлення про помилку.
+// // У блоку finally встановлює isLoading в false після завершення операції.
+
+
+// //onLoadMoreImage:
+// //Збільшує значення page, коли натискається кнопка "Завантажити ще".
+
+
+// //onOpenModal:
+// // Відкриває модальне вікно з вказаним URL largeImage.
+// // Викликає toggleModal для оновлення стану showModal.
+
+
+// //toggleModal
+
+// //Метод рендеру:
